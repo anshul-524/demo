b0131257cc6761f13eabf294a4e2034b58f046a0
anshul 524 <anshul0524@gmail.com>
2024-07-14 10:33:12 +0530

    nav bar add

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navbtn = () => {
+    document.getElementById("nav-bar").classList.toggle("start-0")
+    document.body.classList.toggle("overflow-hidden")
+}
+// function navbarHandler() {
+//     document.getElementById("myTopnav").classList.toggle('show_navbar');
+// }
